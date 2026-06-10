c6ea2972e90b8624f9737e3ae94509faa233cbb4
Maria Eduarda Lima Fernandes De Oliveira <pet0123559@aluno.firjansenaisesi.com.br>
2026-06-09 21:28:09 -0300

    primeiro commit

---
+++ src/pages/Veiculos/Veiculos.jsx
@@ -0,0 +1,7 @@
+const Veiculos = () => {
+    return<div>
+        <h1>Veiculos</h1>
+    </div>
+}
+
+export default Veiculos
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import App from "../App";
+import { Login } from "../pages/Login/Login";
+import { Home } from "../pages/Home/Home";
+import { Motos } from "../pages/Motos/Motos";
+import { Veiculos } from "../pages/Veiculos/Veiculos";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route element={<App />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/motos" element={<Motos />} />
+        <Route path="/veiculos" element={<Veiculos />} />
+      </Route>
+    </Routes>
+  );
+}
